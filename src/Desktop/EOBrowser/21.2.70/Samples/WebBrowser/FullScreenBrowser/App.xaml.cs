f8332f3a54c914dd088bd5483ee3427c5d1e8c31
yangzhou <angenal@hotmail.com>
2021-11-06 10:10:35 +0800

    update

--- src/Desktop/EOBrowser/21.2.70/Samples/WebBrowser/FullScreenBrowser/App.xaml.cs
+++ src/Desktop/EOBrowser/21.2.70/Samples/WebBrowser/FullScreenBrowser/App.xaml.cs
@@ -1,6 +1,8 @@
+using EO.WebEngine;
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text;
 using System.Windows;
 using WindowsWPF.Controls;
 
@@ -23,16 +25,18 @@ namespace FullScreenBrowser
             TransparentSplash.SetBackgroundImage(FullScreenBrowser.Properties.Resources.SplashImage);
             TransparentSplash.BeginDisplay();
 
-            //Set default options
-            EO.Wpf.Runtime.AddLicense(FullScreenBrowser.Properties.Resources.L21);
-            EO.WebBrowser.Runtime.AddLicense(FullScreenBrowser.Properties.Resources.L21);
-            EO.WebEngine.EngineOptions options = EO.WebEngine.EngineOptions.Default;
-            //Disable spell checker
-            options.DisableSpellChecker = true;
-
             //Get the main exe folder
             string exePath = new Uri(Assembly.GetExecutingAssembly().GetName().CodeBase).LocalPath;
             ExeDir = Path.GetDirectoryName(exePath);
+
+            #region Set default web engine options
+            EO.Wpf.Runtime.AddLicense(FullScreenBrowser.Properties.Resources.L21);
+            EO.WebBrowser.Runtime.AddLicense(FullScreenBrowser.Properties.Resources.L21);
+            EngineOptions engine = EngineOptions.Default;
+            //Sets additional plugin directories.
+            //engine.AdditionalPluginsDirs = new string[] { Path.Combine(ExeDir, "plugins") };
+            //Disable spell checker
+            engine.DisableSpellChecker = true;
 
             //Uncomment the following two lines to use eowp.exe.
             //See here for more details:
@@ -41,26 +45,55 @@ namespace FullScreenBrowser
             //EO.Base.Runtime.InitWorkerProcessExecutable(eowpPath);
 
             //Clean up cache folders for older versions
-            EO.WebEngine.Engine.CleanUpCacheFolders(EO.WebEngine.CacheFolderCleanUpPolicy.OlderVersionOnly);
+            Engine.CleanUpCacheFolders(CacheFolderCleanUpPolicy.OlderVersionOnly);
 
             //Set remote debugging port. You only need this line if you
             //wish to use the remote debugging feature. You may need to
             //use a different port if this port is already in use on your system
-            options.RemoteDebugPort = 1234;
-            //options.CustomUserAgent = FullScreenBrowser.Properties.Resources.UserAgent;
-            //System.Diagnostics.Debug.WriteLine(" >> Default::RemoteDebugPort: " + options.RemoteDebugPort);
-            System.Diagnostics.Debug.WriteLine(" >> Default::Custom-UserAgent: " + options.CustomUserAgent);
+            engine.RemoteDebugPort = 1234;
+            //engine.CustomUserAgent = FullScreenBrowser.Properties.Resources.UserAgent;
+            //System.Diagnostics.Debug.WriteLine(" >> Default::RemoteDebugPort: " + engine.RemoteDebugPort);
+            System.Diagnostics.Debug.WriteLine(" >> Default::Custom-UserAgent: " + engine.CustomUserAgent);
 
             //By default remote debugging only accepts connection from localhost.
             //Set this property to true allows you to connect to remote debugging server from another computer.
             //Do not use this option in actual production application
-            //options.RemoteDebugAnyAddress = true;
+            //engine.RemoteDebugAnyAddress = true;
 
             //Uncomment this line to support proprietary media formats.
             //See here for more details:
             //https://www.essentialobjects.com/doc/webbrowser/advanced/html5.aspx
-            //options.AllowProprietaryMediaFormats();
+            //engine.AllowProprietaryMediaFormats();
+            #endregion
 
+            #region Set default web browser options
+            BrowserOptions options = new BrowserOptions();
+            //Sets a value indicating whether JavaScript should be allowed.
+            options.AllowJavaScript = true;
+            //Sets a value indicating whether accessing clipboard from JavaScript should be allowed.
+            options.AllowJavaScriptAccessClipboard = true;
+            //Sets a value indicating whether using JavaScript to close a window that was not opened with JavaScript should be allowed.
+            options.AllowJavaScriptCloseWindow = false;
+            //Sets a value indicating whether pasting from clipboard is allowed.
+            options.AllowJavaScriptDOMPaste = false;
+            //Sets a value indicating whether any plug-in can be loaded.
+            options.AllowPlugins = true;
+            //Sets a value to determine whether user can use Ctrl + mouse wheel or +/- to zoom the current view.
+            options.AllowZooming = false;
+            //Sets the default page encoding.
+            options.DefaultEncoding = Encoding.UTF8; // Encoding.GetEncoding("ISO-8859-1");
+            //Sets a value indicating whether to enforce same-origin policy.
+            //Default it does not allow JavaScript code from one site to access contents of a page from another site.
+            options.EnableWebSecurity = false;
+            //Sets a value indicating whether the browser engine should automatically load images.
+            options.LoadImages = true;
+            //Sets the additional style sheets to be applied to the document.
+            //options.UserStyleSheet = "body { font-family: \"Microsoft Yahei\", Verdana, Arial, Helvetica, sans-serif !important;}";
+
+            engine.SetDefaultBrowserOptions(options);
+            #endregion
+
+            //Startup main window
             MainWnd = new MainWindow();
             MainWnd.Show();
         }
