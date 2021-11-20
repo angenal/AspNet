1f4720ffe25b90233aa5bd9780ab2af196e481f0
yangzhou <angenal@hotmail.com>
2021-11-20 18:01:18 +0800

    update

--- src/Library/WindowsWPF/Windows/Registry.cs
+++ src/Library/WindowsWPF/Windows/Registry.cs
@@ -3,11 +3,23 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
+using System.Threading;
 
 namespace System.Windows
 {
+    /// <summary>
+    /// 注册表工具类
+    /// </summary>
     public static class RegistryTool
     {
+        const string DefaultName = "";
+        const string PrefixShortcut = @"SOFTWARE\Classes\";
+        const string CommandShortcut = @"shell\open\command";
+        // whileSecurity: Loop together until the safety problem is solved
+        const long LoopTimesForLabel = 10; // Loop times
+        const int MillisecondsTimeoutForLabel = 500;
+
         /// <summary>
         /// 创建快捷方式(为当前应用程序)
         /// </summary>
@@ -27,9 +39,6 @@ namespace System.Windows
             RegistShortcut(name, path, protocol, icon, description);
         }
 
-        private const string PrefixShortcut = @"SOFTWARE\Classes\";
-        private const string CommandShortcut = @"shell\open\command";
-
         /// <summary>
         /// 创建快捷方式 abc://api.abc.com/act
         /// </summary>
@@ -38,62 +47,114 @@ namespace System.Windows
         /// <param name="protocol">abc</param>
         /// <param name="icon">C:\Users\Administrator\AppData\Roaming\abc\app.ico</param>
         /// <param name="description">Application Description</param>
-        public static void RegistShortcut(string name, string path, string protocol, string icon, string description = null)
+        /// <param name="whileSecurity">一起循环直到安全问题解决</param>
+        public static void RegistShortcut(string name, string path, string protocol, string icon, string description = null, bool whileSecurity = true)
         {
             if (string.IsNullOrEmpty(name)) throw new ArgumentException("You must provide the argument.", nameof(name));
             if (string.IsNullOrEmpty(path)) throw new ArgumentException("You must provide the argument.", nameof(path));
             if (string.IsNullOrEmpty(protocol)) throw new ArgumentException("You must provide the argument.", nameof(protocol));
 
-            RegistryKey rk = Registry.CurrentUser.CreateSubKey(PrefixShortcut + name), k0 = null, k1 = null;
-            rk.SetValue("", "URL:" + protocol);
-            rk.SetValue("URL Protocol", "");
-            //rk.SetValue("", protocol + "Protocol");
-            //rk.SetValue("URL Protocol", exePath);
-
-            if (!string.IsNullOrEmpty(description))
+            long location = 0; // Loop seed
+        whileSecurityLabel1: // Loop start
+            try
             {
-                k0 = rk.CreateSubKey("Application");
-                k0.SetValue("", description);
-            }
+                RegistryKey rk = Registry.CurrentUser.CreateSubKey(PrefixShortcut + name), k0 = null, k1 = null;
+                rk.SetValue(DefaultName, "URL:" + protocol);
+                rk.SetValue("URL Protocol", DefaultName);
+                //rk.SetValue("", protocol + "Protocol");
+                //rk.SetValue("URL Protocol", exePath);
 
-            if (!string.IsNullOrEmpty(icon))
+                if (!string.IsNullOrEmpty(description))
+                {
+                    k0 = rk.CreateSubKey("Application");
+                    k0.SetValue(DefaultName, description);
+                }
+
+                if (!string.IsNullOrEmpty(icon))
+                {
+                    k1 = rk.CreateSubKey("DefaultIcon");
+                    k1.SetValue(DefaultName, icon);
+                }
+
+                RegistryKey k2 = rk.CreateSubKey(CommandShortcut);
+                k2.SetValue(DefaultName, path);
+
+                rk.Close();
+                if (k0 != null) k0.Close();
+                if (k1 != null) k1.Close();
+                k2.Close();
+            }
+            catch (SecurityException)
             {
-                k1 = rk.CreateSubKey("DefaultIcon");
-                k1.SetValue("", icon);
+                while (whileSecurity && Interlocked.Increment(ref location) < LoopTimesForLabel)
+                {
+                    Thread.Sleep(MillisecondsTimeoutForLabel);
+                    goto whileSecurityLabel1; // Loop end
+                }
             }
-
-            RegistryKey k2 = rk.CreateSubKey(CommandShortcut);
-            k2.SetValue("", path);
-
-            rk.Close();
-            if (k0 != null) k0.Close();
-            if (k1 != null) k1.Close();
-            k2.Close();
         }
 
-        public static bool ExistsShortcut(string name)
+        /// <summary>
+        /// 检查注册表键值对是否存在
+        /// </summary>
+        /// <param name="name">注册表键名</param>
+        /// <param name="whileSecurity">一起循环直到安全问题解决</param>
+        /// <returns></returns>
+        public static bool ExistsShortcut(string name, bool whileSecurity = true)
         {
             bool ok = false;
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey(PrefixShortcut + name);
-            if (rk != null)
+            long location = 0; // Loop seed
+        whileSecurityLabel2: // Loop start
+            try
             {
-                string v = rk.GetValue("")?.ToString();
-                ok = v != null && v.StartsWith("URL:");
-                rk.Close();
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey(PrefixShortcut + name);
+                if (rk != null)
+                {
+                    string v = rk.GetValue(DefaultName)?.ToString();
+                    ok = v != null && v.StartsWith("URL:");
+                    rk.Close();
+                }
+            }
+            catch (SecurityException)
+            {
+                while (whileSecurity && Interlocked.Increment(ref location) < LoopTimesForLabel)
+                {
+                    Thread.Sleep(MillisecondsTimeoutForLabel);
+                    goto whileSecurityLabel2; // Loop end
+                }
             }
             return ok;
         }
 
-        public static string GetShortcut(string name)
+        /// <summary>
+        /// 获取打开快捷方式执行的命令
+        /// </summary>
+        /// <param name="name">注册表键名</param>
+        /// <param name="whileSecurity">一起循环直到安全问题解决</param>
+        /// <returns></returns>
+        public static string GetShortcut(string name, bool whileSecurity = true)
         {
             string v = null;
-            RegistryKey rk = Registry.CurrentUser.OpenSubKey(PrefixShortcut + name);
-            if (rk != null)
+            long location = 0; // Loop seed
+        whileSecurityLabel3: // Loop start
+            try
             {
-                RegistryKey k2 = rk.OpenSubKey(CommandShortcut);
-                if (k2 != null) v = k2.GetValue("")?.ToString();
-                rk.Close();
-                if (k2 != null) k2.Close();
+                RegistryKey rk = Registry.CurrentUser.OpenSubKey(PrefixShortcut + name);
+                if (rk != null)
+                {
+                    RegistryKey k2 = rk.OpenSubKey(CommandShortcut);
+                    if (k2 != null) v = k2.GetValue(DefaultName)?.ToString();
+                    rk.Close();
+                    if (k2 != null) k2.Close();
+                }
+            }
+            catch (SecurityException)
+            {
+                while (whileSecurity && Interlocked.Increment(ref location) < LoopTimesForLabel)
+                {
+                    Thread.Sleep(MillisecondsTimeoutForLabel);
+                    goto whileSecurityLabel3; // Loop end
+                }
             }
             return v;
         }
